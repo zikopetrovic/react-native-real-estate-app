24a96a28615b55d95ec54bcbcb17e9c8d7a74013
Ziko Petrovic <zpetrovic96@outlook.com>
2025-03-06 22:44:57 +0100

    Install NativeWind and configure it.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
