af2310d0811dbc7a34de7eb7bd5df89e9c42be0a
Ziko Petrovic <zpetrovic96@outlook.com>
2025-03-06 23:41:22 +0100

    Configure fonts, icons, and images. Configure base theme for the app.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,7 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text className="font-bold text-lg my-10">Welcome to ReState</Text>
+      <Text className="font-bold text-lg my-10 font-rubik text-3xl">
+        Welcome to ReState
+      </Text>
       <Link href="/sign-in">Sign In</Link>
       <Link href="/explore">Explore</Link>
       <Link href="/profile">Profile</Link>
