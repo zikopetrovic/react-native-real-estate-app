c8e1633a32eaaa42278996ebdc01c5ae9fc9be8b
Ziko Petrovic <zpetrovic96@outlook.com>
2025-03-08 23:29:17 +0100

    Design Profile page, and handle logout.

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,12 +1,111 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  ImageSourcePropType,
+  Alert,
+} from "react-native";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { settings } from "@/constants/data";
+import { useGlobalContext } from "@/lib/global-provider";
+import { logout } from "@/lib/appwrite";
 
-const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
+interface SettingsItemProps {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress?: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
 }
 
-export default Profile
+const SettingsItem = ({
+  icon,
+  title,
+  onPress,
+  textStyle,
+  showArrow = true,
+}: SettingsItemProps) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className="flex flex-row items-center justify-between py-3"
+  >
+    <View className="flex flex-row items-center gap-3">
+      <Image source={icon} className="size-6" />
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>
+        {title}
+      </Text>
+    </View>
+
+    {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+  </TouchableOpacity>
+);
+
+const Profile = () => {
+  const { user, refetch } = useGlobalContext();
+
+  const handleLogout = async () => {
+    const result = await logout();
+
+    if (result) {
+      Alert.alert("Success", "You have been logged out successfully.");
+      refetch();
+    } else {
+      Alert.alert("Error", "An error occurred while logging out.");
+    }
+  };
+
+  return (
+    <SafeAreaView className="h-full bg-white">
+      <View className="flex flex-row items-center justify-between mt-5 px-7">
+        <Text className="text-xl font-rubik-bold">Profile</Text>
+        <Image source={icons.bell} className="size-5" />
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerClassName="pb-32 px-7"
+      >
+        <View className="flex-row justify-center flex mt-5">
+          <View className="flex flex-col items-center relative mt-5">
+            <Image
+              source={{ uri: user?.avatar }}
+              className="size-44 relative rounded-full"
+            />
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
+            </TouchableOpacity>
+
+            <Text className="text-2xl font-rubik-bold mt-2">{user?.name}</Text>
+          </View>
+        </View>
+
+        <View className="flex flex-col mt-10">
+          <SettingsItem icon={icons.calendar} title="My Bookings" />
+          <SettingsItem icon={icons.wallet} title="Payments" />
+        </View>
+
+        <View className="flex flex-col mt-5 border-t pt-5 border-primary-200">
+          {settings.slice(2).map((item, index) => (
+            <SettingsItem key={index} {...item} />
+          ))}
+        </View>
+
+        <View className="flex flex-col border-t mt-5 pt-5 border-primary-200">
+          <SettingsItem
+            icon={icons.logout}
+            title="Logout"
+            textStyle="text-danger"
+            showArrow={false}
+            onPress={handleLogout}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default Profile;
