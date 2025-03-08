cf015ed82766db676ecf0606206739af0e3db330
Ziko Petrovic <zpetrovic96@outlook.com>
2025-03-08 22:07:45 +0100

    add tabs for the home page to display Home, Search and Profile tabs

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,85 @@
+import { View, Text, Image } from "react-native";
+import React from "react";
+import { Tabs } from "expo-router";
+
+import icons from "@/constants/icons";
+
+const TabIcon = ({
+  focused,
+  icon,
+  title,
+}: {
+  focused: boolean;
+  icon: any;
+  title: string;
+}) => (
+  <View className="flex-1 mt-3 flex flex-col items-center">
+    <Image
+      source={icon}
+      tintColor={focused ? "#0061ff" : "#666876"}
+      resizeMode="contain"
+      className="size-6"
+    />
+    <Text
+      className={`${
+        focused
+          ? "text-primary-300 font-rubik-medium"
+          : "text-black-200 font-rubik"
+      } text-xs w-full text-center mt-1`}
+    >
+      {title}
+    </Text>
+  </View>
+);
+
+const TabsLayout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          backgroundColor: "white",
+          position: "absolute",
+          borderTopColor: "#0061FF1A",
+          borderTopWidth: 1,
+          minHeight: 70,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.home} focused={focused} title="Home" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: "Explore",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.search} focused={focused} title="Explore" />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon icon={icons.person} focused={focused} title="Profile" />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -13,10 +13,6 @@ export default function Index() {
       <Text className="font-bold text-lg my-10 font-rubik text-3xl">
         Welcome to ReState
       </Text>
-      <Link href="/sign-in">Sign In</Link>
-      <Link href="/explore">Explore</Link>
-      <Link href="/profile">Profile</Link>
-      <Link href="/properties/1">Property</Link>
     </View>
   );
 }
